343fe018ed1414ddf4b182f79369697bc5ad840f
Rafael <webdeveloperrm@gmail.com>
2021-02-28 23:38:13 -0300

    Pagina contato

---
+++ pages/contato.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Cabecalho from '../src/components/commons/cabecalho';
+import Rodape from '../src/components/commons/rodape';
+
+export default function PageContato(){
+    return (
+        <>
+         <div>
+         <Cabecalho />  
+             Pagina contato
+             <Rodape />  
+         </div>
+        </>
+    )
+}
